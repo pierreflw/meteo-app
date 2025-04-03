34832815392f548fed075d67531cc57c8db564fb
pfw <pfw@mcbookpfw.local>
2025-04-04 00:53:59 +0200

    changing port

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`🌐 Serveur lancé sur http://localhost:${PORT}`);
 });
